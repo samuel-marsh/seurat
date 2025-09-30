8d566285c8d0b9aaf7ab732f3e1bd1723134e74d
Samuel Marsh <38284410+samuel-marsh@users.noreply.github.com>
2025-09-30 10:57:16 -0400

    aes_string LabelPoints

--- R/visualization.R
+++ R/visualization.R
@@ -6128,7 +6128,7 @@ LabelClusters <- function(
 #' @return A ggplot object
 #'
 #' @importFrom ggrepel geom_text_repel
-#' @importFrom ggplot2 geom_text aes_string
+#' @importFrom ggplot2 geom_text
 #' @export
 #' @concept visualization
 #'
@@ -6171,7 +6171,7 @@ LabelPoints <- function(
     }
   }
   plot <- plot + geom.use(
-    mapping = aes_string(x = xynames$x, y = xynames$y, label = 'labels'),
+    mapping = aes(x = .data[[xynames$x]], y = .data[[xynames$y]], label = .data[['labels']]),
     data = label.data,
     nudge_x = xnudge,
     nudge_y = ynudge,
